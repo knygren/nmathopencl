c88aa2e76fd56543b8dd452e04363155ba66dafd
knygren <kjell.a.nygren@gmail.com>
2026-03-24 07:21:20 -0400

    Migrate dataset documentation to Roxygen and dedicated example files

---
+++ R/data-AMI.R
@@ -0,0 +1,26 @@
+#' Amitriptyline overdose data
+#'
+#' Data with information on 17 overdoses of the drug amitriptyline
+#'
+#' @format This data frame contains the following columns:
+#' \describe{
+#'   \item{\code{TOT}}{total TCAD plasma level}
+#'   \item{\code{AMI}}{amount of amitriptyline present in the TCAD plasma level}
+#'   \item{\code{GEN}}{gender (male = 0, female = 1)}
+#'   \item{\code{AMT}}{amount of drug taken at time of overdose}
+#'   \item{\code{PR}}{PR wave measurement}
+#'   \item{\code{DIAP}}{diastolic blood pressure}
+#'   \item{\code{QRS}}{QRS wave measurement}
+#' }
+#'
+#' @details Details to be added...
+#'
+#' @references UVA Example - Add Reference Here.
+#'
+#' @usage data(AMI)
+#'
+#' @example inst/examples/Ex_AMI.R
+#'
+#' @keywords datasets
+#' @concept Bayesian Binomial Regression
+"AMI"
